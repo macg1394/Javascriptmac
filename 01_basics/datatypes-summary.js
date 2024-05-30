aa3cf208d1068d12d0218a986da300514394da0f
macg1394 <120573108+macg1394@users.noreply.github.com>
2024-05-30 05:12:39 +0000

    day 4

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,55 @@
+// //  Primitive
+
+// //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+// const score = 100 //number
+// const scoreValue = 100.3 //number
+
+// const isLoggedIn = false //boolean
+// const outsideTemp = null //object
+// let userEmail; //undefined
+
+// const id = Symbol('123') //symbol
+// const anotherId = Symbol('123') //symbol
+
+// console.log(id === anotherId); //false
+
+//  const bigNumber = 3456543576654356754n //bigint
+
+
+
+// // Reference (Non primitive)
+
+// // Array, Objects, Functions
+
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh", //object
+//     age: 22,
+// }
+
+// const myFunction = function(){ //function ko variable mein assign kr skte hai
+//     console.log("Hello world");
+// }
+
+console.log(typeof apple); //undefined
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
+https://youtu.be/7gwc-1czolw?si=X_al-HdAwVoY7Syf  //ye dekh lena jldi iske baad primitive stack aur non
+//primitive heap k form mein store ho jate hai
+
