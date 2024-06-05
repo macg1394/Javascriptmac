a2887b21d3cd642cd6eff0ab8f06cd5d63b38593
macg1394 <120573108+macg1394@users.noreply.github.com>
2024-06-05 12:53:16 +0000

    objects over revise again

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,41 @@
+// singleton-constructor se bnega mtb singleton ,literal se singleton nhi -bnega
+
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1") //symbol declare
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    //mySym: "mykey1", console.log(typeof JsUser[mySym])-string
+    [mySym]: "mykey1", //imp-symbol jaise use krne ke liye sqr brckt mein declare krna pdega
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+ console.log(JsUser[mySym])
+
+// JsUser.email = "hitesh@chatgpt.com" //change 
+// // Object.freeze(JsUser) //user change na krpaye
+// JsUser.email = "hitesh@microsoft.com"
+// // console.log(JsUser);-puri details ke liye
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+     console.log(`Hello JS user, ${this.name}`); //jb koi value chahiye hoti hai na 
+// to ` lagana string ke jagah
+}
+
+console.log(JsUser.greeting); //[Function (anonymous)]
+console.log(JsUser.greetingTwo());
---
+++ 02_basics/04_objects2.js
@@ -0,0 +1,89 @@
+// const tinderUser = new Object() -singleton
+const tinderUser = {}//-non singleton
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "hitesh",
+            lastname: "choudhary"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }-ye nhi krna hai
+ //const obj3 = Object.assign({}, obj1, obj2, obj4)
+//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+//const obj3 = {...obj1, ...obj2}//ye use krenge
+//{ '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+ //console.log(obj3);
+
+
+const users = [ //array ke andr objects
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser)); //keys ka array
+// console.log(Object.values(tinderUser)); values ka array
+// console.log(Object.entries(tinderUser)); ek key aur uski value ka array phir dusra
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); //true
+
+//----destructuring----
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+const {courseInstructor} = course //ab direct courseINst..ko use kr skte without writing course.
+const {courseInstructor: instructor} = course //courseInstructor ka short form hai instructor
+
+// console.log(courseInstructor);
+console.log(instructor);
+//{}-destructuring kr rhe hai mtb agar kahi dikhe to
+
+
+//ye niche hai json
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[ //array
+    {},
+    {},
+    {}
+]
+
+//json formatter
+//api are sent in json format,earlier in xml 
