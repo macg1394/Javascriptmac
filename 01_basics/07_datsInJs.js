20bbf190d629753d46cce692c441355990a53569
macg1394 <120573108+macg1394@users.noreply.github.com>
2024-06-05 04:08:12 +0000

    another day

--- 01_basics/07_datsInJs.js
+++ 01_basics/07_datsInJs.js
@@ -4,23 +4,23 @@ let myDate = new Date()
 // console.log(myDate.toString());
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
-// console.log(typeof myDate);
+// console.log(typeof myDate); //object
 
-// let myCreatedDate = new Date(2023, 0, 23)
-// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date(2023, 0, 23) //console.log(myCreatedDate.toLocaleString());-Mon Jan 23 2023
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3) 5.03 time mein aayega
 // let myCreatedDate = new Date("2023-01-14")
 let myCreatedDate = new Date("01-14-2023")
 // console.log(myCreatedDate.toLocaleString());
 
 let myTimeStamp = Date.now()
-
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime());
-// console.log(Math.floor(Date.now()/1000));
+//The Date.now() static method returns the number of milliseconds elapsed since the epoch, which is defined as the midnight at the beginning of January 1, 1970, UTC.
+// console.log(myTimeStamp);  //in milisec
+// console.log(myCreatedDate.getTime()); //1970 se myCreated date tk
+// console.log(Math.floor(Date.now()/1000)); //imp -into second
 
 let newDate = new Date()
 console.log(newDate);
-console.log(newDate.getMonth() + 1);
+console.log(newDate.getMonth() + 1); //january 0 se start hota hai
 console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
---
+++ 02_basics/01_arrays1.js
@@ -0,0 +1,42 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9) //start mein add
+// myArr.shift() // The shift() method of Array instances removes the first element from an array and returns that removed element.
+
+// console.log(myArr.includes(9)); //false
+// console.log(myArr.indexOf(3)); //returns -1 if element is not there
+
+// const newArr = myArr.join()
+
+// console.log(myArr); [0,1,2,3,4,5]
+// console.log( newArr); myArray ko string mein tbdil kr diya with comma separated 0,1,2,3,4,5
+//typeof newAre=string
+
+// slice, splice
+
+console.log("A ", myArr);
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
+// A  [ 0, 1, 2, 3, 4, 5 ]
+// [ 1, 2 ]
+// B  [ 0, 1, 2, 3, 4, 5 ]
+// C  [ 0, 4, 5 ]splice manipulated original array
+// [ 1, 2, 3 ]
