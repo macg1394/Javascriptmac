289446e60854825657a90826e78c01445bb016e1
macg1394 <120573108+macg1394@users.noreply.github.com>
2024-06-14 12:22:35 +0000

    14th june

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,7 +1,7 @@
 const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNumers.map( (num) => { return num + 10})
-
+//ek array return hoga jisme saari values mein +10 hokr aayega
 const newNums = myNumers
                 .map((num) => num * 10 )
                 .map( (num) => num + 1)
